d38904d6f7ad346b0cc692e3e7f5f391ad85fa3b
kamlesh sahu <vasubantikamlesh@gmail.com>
2020-12-24 08:57:40 +0530

    NextPermutation

---
+++ scaler/src/ArrayAndMaths/NextPermutation.java
@@ -0,0 +1,35 @@
+package ArrayAndMaths;
+
+public class NextPermutation {
+
+    public int[] nextPermutation(int[] number) {
+
+        int i = 0;
+        for (i = number.length - 1; i > 0; i--)
+            if (number[i - 1] < number[i])
+                break;
+
+
+        if (i != 0) {
+
+            int x = number[i - 1], smallest = i;
+            for (int j = i + 1; j < number.length; j++)
+                if (number[j] > x && number[j] <= number[smallest])
+                    smallest = j;
+
+            int temp = number[i - 1];
+            number[i - 1] = number[smallest];
+            number[smallest] = temp;
+
+            Arrays.sort(number, i, number.length);
+        } else {
+            Arrays.sort(number);
+        }
+        return number;
+    }
+};
+
+//day 2 , hw
+//difficulty = medium , maths, plot 2D graph
+//brute force:
+//t.c = O(n)
