bea0655a88f01c0c0c6b13f0349f83c27528439c
kamlesh sahu <vasubantikamlesh@gmail.com>
2021-02-13 09:38:33 -0800

    RandomisedSet

---
+++ scaler/src/Array/RandomisedSet.java
@@ -0,0 +1,51 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class RandomisedSet {
+
+    ArrayList<Integer> nums;
+    HashMap<Integer, Integer> locs;
+    java.util.Random rand = new java.util.Random();
+    /** Initialize your data structure here. */
+    public void RandomizedSet() {
+        nums = new ArrayList<Integer>();
+        locs = new HashMap<Integer, Integer>();
+    }
+
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        boolean contain = locs.containsKey(val);
+        if ( contain ) return false;
+        locs.put( val, nums.size());
+        nums.add(val);
+        return true;
+    }
+
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        boolean contain = locs.containsKey(val);
+        if ( ! contain ) return false;
+        int loc = locs.get(val);
+        if (loc < nums.size() - 1 ) { // not the last one than swap the last one with this val
+            int lastone = nums.get(nums.size() - 1 );
+            nums.set( loc , lastone );
+            locs.put(lastone, loc);
+        }
+        locs.remove(val);
+        nums.remove(nums.size() - 1);
+        return true;
+    }
+
+    /** Get a random element from the set. */
+    public int getRandom() {
+        return nums.get( rand.nextInt(nums.size()) );
+    }
+}
+
+// thinking : medium
+// impl : easy
+// clever logic, think how can you reduce the t.c of removing element of array + map structure,
+// will swapping with last element work??
+// t.c = O(1)
