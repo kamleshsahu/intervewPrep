9e15d580947c8f7c4552fb4a61d488a243eda5d6
Kamlesh Sahu <vasubantikamlesh@gmail.com>
2021-03-03 23:44:23 +0530

    LinkedList in tree

---
+++ scaler/src/Tree/LinkedListInTree.java
@@ -0,0 +1,21 @@
+package Tree;
+
+public class LinkedListInTree {
+
+    public static boolean check(TreeNode root, ListNode head) {
+        if (head == null) return true;
+        if (root == null) return false;
+        if (root.val != head.val) return false;
+        return check(root.left, head.next) || check(root.right, head.next);
+    }
+
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if (root == null) return false;
+        return check(root, head) || isSubPath(head, root.left) || isSubPath(head, root.right);
+    }
+}
+
+// think of all test cases, read the question properly
+
+
+
